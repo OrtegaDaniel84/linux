f11161c559c9f7164a9756603dde1373cf340149
Dany <129864433+OrtegaDaniel84@users.noreply.github.com>
2026-01-12 16:29:06 +0100

    Update setup script filename

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #Install prerequisites
 # apt install sudo git
 # echo "daniel  ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+# git clone https://github.com/OrtegaDaniel84/linux.git configs
+# sh /configs/setup.sh
 
 # Install packages
 sudo apt install -y --no-install-recommends \
   xorg openbox obconf polybar rofi alacritty \
   x11vnc novnc websockify \
   fonts-dejavu-core curl xvfb python3-xdg \
-  network-manager-gnome vim feh
-
-git clone https://github.com/OrtegaDaniel84/linux.git configs
+  network-manager-gnome vim feh wmctrl \
+  lightdm lightdm-gtk-greeter
 
 cp -r configs/.config/* .config/
 chmod +x .config/openbox/autostart 
@@ -18,6 +19,7 @@ chmod +x .config/polybar/polywins.sh
 mkdir -p /home/daniel/Pictures
 cp configs/bg.jpg Pictures/
 
+sudo cp configs/novnc/index.html /usr/share/novnc/index.html
 sudo cp configs/interfaces /etc/network/interfaces 
 sudo cp configs/openbox-novnc.service /etc/systemd/system/openbox-novnc.service
 sudo mkdir -p /etc/polkit-1/rules.d/
@@ -27,22 +29,16 @@ sudo sed -i 's/managed=false/managed=true/' /etc/NetworkManager/NetworkManager.c
 
 sudo usermod -aG netdev daniel
 
-sudo cp /usr/share/novnc/vnc_lite.html /usr/share/novnc/index.html
-
-
 sudo systemctl daemon-reload
-sudo systemctl enable --now NetworkManager
-sudo systemctl enable --now openbox-novnc.service
+sudo systemctl enable lightdm
+sudo systemctl enable NetworkManager
+sudo systemctl enable openbox-novnc.service
 
-sudo systemctl restart polkit
-sudo systemctl restart NetworkManager
-sudo systemctl restart openbox-novnc.service
+#reboot reboot
 
-# Change default terminal to gnome-terminal
-sudo update-alternatives --config x-terminal-emulator
 
 # Install Google Chrome
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/google.asc >/dev/null
-vi /etc/apt/sources.list << deb http://dl.google.com/linux/chrome/deb/ stable main
-sudo apt install google-chrome-stable
+# wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/google.asc >/dev/null
+# vi /etc/apt/sources.list << deb http://dl.google.com/linux/chrome/deb/ stable main
+# sudo apt install google-chrome-stable
 
